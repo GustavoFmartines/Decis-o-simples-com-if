f7b9f4bc128b5aa5c14376301e1f19fdd5ba1272
Gustavo Figueiredo Martines <gustavo.martines2003@outlook.com>
2025-05-09 19:40:36 -0300

    Feito testes basicos com if e else

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+    Decimal p1, p2, media;
+
+Console.Write("Nota da P1: ");
+    p1 = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Nota da P2:");
+    p2 = Convert.ToDecimal(Console.ReadLine());
+
+        media = p1 + p2 / 2;
+
+Console.WriteLine($"A média do aluno é: {media}");
+
+if (media >= 5)
+    Console.WriteLine("Aprovado!!!");
+
+else
+{
+    Console.WriteLine("Reprovado! :(");
+};
